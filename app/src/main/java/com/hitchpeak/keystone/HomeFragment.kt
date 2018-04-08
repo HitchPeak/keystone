992b4c363a68f044d0ebcdd3573c4bac5f3c967d
Antoni Mleczko <antos.mleczko@gmail.com>
2018-04-08 15:33:52 +0200

    UI stub

---
+++ app/src/main/java/com/hitchpeak/keystone/HomeFragment.kt
@@ -0,0 +1,69 @@
+package com.hitchpeak.keystone
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [HomeFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class HomeFragment : Fragment(), OnMapReadyCallback {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        map?.onCreate(null)
+        map?.onResume()
+        map?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        MapsInitializer.initialize(context)
+
+        googleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = HomeFragment()
+    }
+}
