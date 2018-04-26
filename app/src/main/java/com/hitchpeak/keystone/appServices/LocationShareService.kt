ed6eea5feeae67f723367428db1bccd99f06febc
Antoni Mleczko <antos.mleczko@gmail.com>
2018-04-26 15:35:44 +0200

    Changed LocationShareService to run on worker thread.

--- app/src/main/java/com/hitchpeak/keystone/appServices/LocationShareService.kt
+++ app/src/main/java/com/hitchpeak/keystone/appServices/LocationShareService.kt
@@ -6,15 +6,19 @@ import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.IBinder
+import android.support.annotation.WorkerThread
 import android.support.v4.app.NotificationCompat
 import android.support.v4.content.ContextCompat
 import com.google.android.gms.location.*
+import com.hitchpeak.keystone.BuildConfig
 import com.hitchpeak.keystone.R
 
 class LocationShareService : Service() {
-    // TODO("Start this on different thread")
 
     protected lateinit var locationProvider: FusedLocationProviderClient
+
+    val channelId = BuildConfig.NOTIF_CHANNEL_ID
+    val notificationId = BuildConfig.LOCATION_NOTIF_ID
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         startLocationSharing()
@@ -24,38 +28,43 @@ class LocationShareService : Service() {
     }
 
     private fun startForegroundNotification() {
-        // TODO("extract this variables somewhere")
-        val channelId = "App"
-        val notificationId = 1234
-        val notification: Notification = NotificationCompat.Builder(this,channelId)
+        val notification: Notification = NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Hitchpeak")
-                .setContentText("We are watching you!")
+                .setContentTitle(getString(R.string.app_title))
+                .setContentText(getString(R.string.notification_text))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build()
 
         startForeground(notificationId, notification)
     }
 
+    @WorkerThread
     private fun startLocationSharing() {
         // The gist of location sharing:
-        locationProvider = LocationServices.getFusedLocationProviderClient(applicationContext)
 
-        val locationRequest = LocationRequest.create()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 5
+        Thread().run {
+            locationProvider = LocationServices.getFusedLocationProviderClient(applicationContext)
 
-        val locationCallback: LocationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                // TODO("Handle location sharing here.")
-                println(locationResult)
+            val locationRequest = LocationRequest.create()
+            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            locationRequest.interval = BuildConfig.LOCATION_SHARE_INTERVAL
+
+            val locationCallback: LocationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    // TODO("Handle location sharing here.")
+                    println(locationResult)
+                }
+            }
+
+            if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                locationProvider.requestLocationUpdates(locationRequest, locationCallback, null)
+            } else {
+                // TODO("Handle case when permission is not granted")
             }
         }
 
-        // TODO("Handle case when permission is not granted")
-        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED)
-            locationProvider.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 
     override fun onBind(intent: Intent): IBinder? {
--- app/src/main/java/com/hitchpeak/keystone/utils/HttpClient.kt
+++ app/src/main/java/com/hitchpeak/keystone/utils/HttpClient.kt
@@ -1,36 +1,22 @@
 package com.hitchpeak.keystone.utils
 
 import android.content.Context
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.Volley
 
 
-class HttpClient constructor(context: Context, baseUrl: String ) {
+class HttpClient constructor(context: Context)  {
 
     companion object {
-        val baseUrl: String = "http://10.0.2.2:8000/"
 
         @Volatile
         var instance : HttpClient? = null
             private set
 
-        fun init(context: Context, baseUrl: String = this.baseUrl) {
+        fun init(context: Context) {
             synchronized(this) {
-                instance = HttpClient(context, baseUrl)
+                instance = HttpClient(context)
             }
         }
 
-        fun makeDefaultErrorHandler() = Response.ErrorListener{ errorMsg: VolleyError -> println("Http error: $errorMsg") }
     }
 
-    val requestQueue: RequestQueue by lazy {
-        Volley.newRequestQueue(context.applicationContext)
-    }
-
-    fun<T> makeRequest(request: Request<T>) {
-        requestQueue.add(request)
-    }
 }
